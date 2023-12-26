003967fc4a7fac323dab4985b2a69f020a94c4cc
zaidkamil <xaidmetamorphos@gmail.com>
2023-12-26 19:11:53 +0530

    searching

--- shop/urls.py
+++ shop/urls.py
@@ -6,8 +6,7 @@ urlpatterns = [
     path('products/all/', views.all_products, name='all_products'),
     path('brands/<slug:brand>/all/', views.brand_products, name='brand_products'),
     path('category/<slug:category>/all/', views.category_products, name='category_products'),
-    path('brand-category/<slug:brand>/<slug:category>/all/', views.brand_category_products, 
-         name='brand_category_products'),
+   
     # searching products
     path('products/search/', views.search_products, name='search_products'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product, Brand, Category
 from .filters import ProductFilter
+from django.contrib import messages
 
 def all_products(request):
     f = ProductFilter(request.GET, queryset=Product.objects.all())
@@ -21,10 +22,26 @@ def brand_products(request, brand):
     })
 
 def category_products(request, category):
-    pass
-
-def brand_category_products(request, brand, category):
-    pass
+    catObj = Category.objects.get(slug=category)
+    products = Product.objects.filter(category=catObj)
+    return render(request,'shop/category_products.html',{
+        'category': catObj,
+        'products': products,
+        'brands': Brand.objects.all(),
+        'categories': Category.objects.all()
+    })
 
 def search_products(request):
-    pass
+    q = request.GET.get('q')
+    if q is None:
+        messages.error(request, 'Please enter something to search!')
+    product_list_1 = Product.objects.filter(name__icontains=q) # name contains term
+    product_list_2 = Product.objects.filter(brand__name__icontains=q) # brand contains term
+    product_list_3 = Product.objects.filter(category__name__icontains=q) # category contains terms
+    # product_list_4 = Product.objects.filter(description__icontains=q) # description contains terms
+    return render(request,'shop/search.html',{
+        'products': product_list_1.union(product_list_2, product_list_3),
+        'brands': Brand.objects.filter(name__icontains=q),
+        'categories': Category.objects.filter(name__icontains=q),
+        'q': q,
+    })
